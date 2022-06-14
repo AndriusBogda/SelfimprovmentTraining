72ea3073f25f0ec7075aba5c005954e109711291
Andrius <andriusbogda1@gmail.com>
2022-06-14 13:48:05 +0300

    Added Switch method

--- SeleniumTrainingCenter/Tests/Bases/BaseTest.cs
+++ SeleniumTrainingCenter/Tests/Bases/BaseTest.cs
@@ -4,12 +4,15 @@ using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Remote;
+using System;
+using System.Collections.Generic;
 
 namespace SeleniumTrainingCenter.Tests.Bases
 {
     public class BaseTest
     {
         private static IWebDriver _driver;
+        private static int _switchState = 0;
 
         private static string _url = @"http://localhost:4444/wd/hub/";
 
@@ -20,7 +23,35 @@ namespace SeleniumTrainingCenter.Tests.Bases
             get => _driver;
         }
 
-        
+        public void Switch(DriverOptions driverOptions)
+        {
+            _switchState++; 
+            if (_switchState == 0)
+            {
+                _driver = new ChromeDriver();
+            }
+            else if (_switchState == 1)
+            {
+                _driver = new RemoteWebDriver(new System.Uri(_url), new ChromeOptions());
+            }
+            else if (_switchState == 2)
+            {
+                var sauceOptions = new Dictionary<string, object>();
+                //sauceOptions.Add("name", TestContext.TestName);
+                sauceOptions.Add("username", Environment.GetEnvironmentVariable("SauceLabsMyUsername", EnvironmentVariableTarget.User));
+                sauceOptions.Add("accessKey", Environment.GetEnvironmentVariable("SauceLabsMyAccessKey", EnvironmentVariableTarget.User));
+
+                var browserOptions = driverOptions;
+                browserOptions.AddAdditionalOption("sauce:options", sauceOptions);
+                var sauceUrl = new Uri("https://ondemand.eu-central-1.saucelabs.com/wd/hub");
+
+                _driver = new RemoteWebDriver(sauceUrl, browserOptions);
+            }
+            else
+            {
+                _switchState = 0;
+            }
+        }
 
         [OneTimeSetUp]
         public void OneTimeSetup()
